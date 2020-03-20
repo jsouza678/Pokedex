0198c2d726ac33048cef2387d48595aa4612179f
João Victor Souza <jsouza678@icloud.com>
2020-03-20 18:53:03 -0300

    Navigation, Menu, Navigation Drawer partially added, Retrofit dataclass added.

---
+++ app/src/main/java/souza/home/com/pokedexapp/network/PokeService.kt
@@ -0,0 +1,4 @@
+package souza.home.com.pokedexapp.network
+
+private const val BASE_URL = "https://pokeapi.co/api/v2/"
+
---
+++ app/src/main/java/souza/home/com/pokedexapp/ui/DetailsPokedex.kt
@@ -0,0 +1,26 @@
+package souza.home.com.pokedexapp.ui
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import souza.home.com.pokedexapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class DetailsPokedex : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details_pokedex, container, false)
+    }
+
+
+}
