688891fd7127809ef546685df5a28c4a2d5e6f0f
João Victor Souza <jsouza678@icloud.com>
2020-04-07 13:28:35 -0300

    Coroutines added to the Home ViewModel

--- app/src/main/java/souza/home/com/pokedexapp/network/PokeService.kt
+++ app/src/main/java/souza/home/com/pokedexapp/network/PokeService.kt
@@ -1,7 +1,9 @@
 package souza.home.com.pokedexapp.network
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -22,13 +24,14 @@ private val moshi = Moshi.Builder()
 
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .baseUrl(BASE_URL)
     .build()
 
 interface PokeService{
     @GET("pokemon/?")
     fun getPokes(@Query("offset") page: Int?):
-            Call<PokeRootProperty>
+            Deferred<PokeRootProperty>
 
     @GET("pokemon/{poke}")
     fun getPokeStats(@Path("poke") poke: String?):
--- app/src/main/java/souza/home/com/pokedexapp/presenter/home/HomePokedexViewModel.kt
+++ app/src/main/java/souza/home/com/pokedexapp/presenter/home/HomePokedexViewModel.kt
@@ -11,6 +11,10 @@ import souza.home.com.pokedexapp.network.PokeRootProperty
 import souza.home.com.pokedexapp.network.model.main_model.Pokemon
 import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 enum class HomePokedexStatus{ LOADING, ERROR, DONE, EMPTY}
 
@@ -30,65 +34,44 @@ class HomePokedexViewModel : ViewModel(){
     val status : LiveData<HomePokedexStatus>
     get() = _status
 
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
 
     init{
-        loadFirstPage()
+        _poke.value = mutableListOf()
+        getPokes()
     }
 
-    private fun loadFirstPage(){
+    private fun getPokes(){
 
         _status.value = HomePokedexStatus.LOADING
 
-        PokeApi.retrofitService.getPokes(page).enqueue(object : Callback<PokeRootProperty> { override fun onFailure(call: Call<PokeRootProperty>, t: Throwable) {
-                    _status.value = HomePokedexStatus.ERROR
-                }
-                override fun onResponse(call: Call<PokeRootProperty>, response: Response<PokeRootProperty>) {
-                    val itemsResponse = response.body()
+        coroutineScope.launch {
+            var getPokesDeferred = PokeApi.retrofitService.getPokes(page)
+            try{
+                val listResult = getPokesDeferred.await() // await is non blocking... without blocking the main thread.
 
-                    _poke.value  = itemsResponse?.results!!
+                _poke.value?.addAll(listResult.results!!)
 
-                    if(_poke.value.isNullOrEmpty()){
-                        _status.value = HomePokedexStatus.EMPTY
-                    }else{
-                        val r = Runnable {
-                            _status.value = HomePokedexStatus.DONE
-                        }
-                        Handler().postDelayed(r, 500)
+              /*  for (i in 0 until length!!) {
+                    _poke.value?.add((response.body()?.results!![i]))
+                }*/
 
-                    }
-
-                }
-            })
-    }
-
-    fun getPage(page: Int){
-        isLoading = true
-        _status.value = HomePokedexStatus.LOADING
-
-        PokeApi.retrofitService.getPokes(page)
-            .enqueue(object : Callback<PokeRootProperty> {
-                override fun onFailure(call: Call<PokeRootProperty>, t: Throwable) {
-                    _status.value = HomePokedexStatus.ERROR
-                }
-
-                override fun onResponse(
-                    call: Call<PokeRootProperty>,
-                    response: Response<PokeRootProperty>
-                ) {
-
-                    val length = response.body()?.results?.size
-
-                    for (i in 0 until length!!) {
-                        _poke.value?.add((response.body()?.results!![i]))
-                    }
-
-                    isLoading = false
-                    val r = Runnable {
+                if(_poke.value.isNullOrEmpty()){
+                    _status.value = HomePokedexStatus.EMPTY
+                }else{
+                   /* val r = Runnable {*/
                         _status.value = HomePokedexStatus.DONE
-                    }
-                    Handler().postDelayed(r, 500)
+/*                    }
+                    Handler().postDelayed(r, 500)*/
+
                 }
-            })
+            }catch(t: Throwable){
+                _status.value = HomePokedexStatus.ERROR
+            }
+        }
+
     }
 
     fun onRecyclerViewScrolled(dy: Int, layoutManager: GridLayoutManager){
@@ -96,7 +79,8 @@ class HomePokedexViewModel : ViewModel(){
             val isItTheListEnd = itIsTheListEnd(layoutManager = layoutManager)
             if(isLoading.not() && isItTheListEnd){
                 page+=20
-                getPage(page)
+                //getPage(page)
+                getPokes()
             }
         }
     }
@@ -107,5 +91,10 @@ class HomePokedexViewModel : ViewModel(){
         val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
 
         return visibleItemCount + firstVisibleItemPosition >= totalItemCount
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Job().cancel()
     }
 }
